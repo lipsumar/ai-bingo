008d6962b24c509db29a71f3f29b582ace77644e
Emmanuel Pire <piremmanuel@gmail.com>
2023-01-15 16:30:18 +0100

    style homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,12 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-  <main>
-    <h1>AI Bingo!</h1>
-    <RouterLink to="/play">Play now</RouterLink>
+  <main class="h-screen flex flex-col items-center justify-center">
+    <h1 class="text-4xl mb-10">🎰 AI Bingo!</h1>
+    <RouterLink
+      to="/play"
+      class="w-40 py-4 font-bold bg-slate-500 text-white text-center rounded-lg"
+      >Play now</RouterLink
+    >
   </main>
 </template>

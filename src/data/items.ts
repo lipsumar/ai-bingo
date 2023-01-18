8856b6897f139337b99a9a7fccce692cb334aac0
Emmanuel Pire <piremmanuel@gmail.com>
2023-01-18 23:23:43 +0100

    27 set of images

--- src/data/items.ts
+++ src/data/items.ts
@@ -4,9 +4,9 @@ export const items = [
     prompt:
       "earth after human extinction, a new beginning, nature taking back the planet, harmony, peace, earth balanced",
     images: [
-      { image: "0/d4c6181-b2e9ec8.jpg", ai: "de" },
-      { image: "0/d4c6181-965eb49.png", ai: "sd" },
-      { image: "blah.jpg", ai: "mj" },
+      { image: "0/d4c6181-9ac231f.jpg", ai: "de" },
+      { image: "0/d4c6181-6d7d5f6.png", ai: "sd" },
+      { image: "0/d4c6181-114a63f.png", ai: "mj" },
     ],
   },
   {
@@ -14,54 +14,258 @@ export const items = [
     prompt:
       "a home built in a huge Soap bubble, windows, doors, porches, awnings, middle of SPACE, cyberpunk lights, Hyper Detail, 8K, HD, Octane Rendering, Unreal Engine, V-Ray, full hd",
     images: [
-      { image: "1/860e7ce-3f2529a.jpg", ai: "de" },
-      { image: "1/860e7ce-7add5b5.png", ai: "sd" },
-      { image: "blah.jpg", ai: "mj" },
+      { image: "1/860e7ce-ac33474.jpg", ai: "de" },
+      { image: "1/860e7ce-14568d7.png", ai: "sd" },
+      { image: "1/860e7ce-d63bee9.png", ai: "mj" },
     ],
   },
   {
     id: "d54a5020c2af64b2772e554c3245668f",
     prompt: "old Japanese cemetery, mystical, overgrown, green fog",
     images: [
-      { image: "2/d54a502-d66ac80.jpg", ai: "de" },
-      { image: "2/d54a502-9ce5aef.png", ai: "sd" },
-      { image: "blah.jpg", ai: "mj" },
+      { image: "2/d54a502-ea13bdd.jpg", ai: "de" },
+      { image: "2/d54a502-e657328.png", ai: "sd" },
+      { image: "2/d54a502-4a32c02.png", ai: "mj" },
     ],
   },
   {
     id: "fa0fa4cbecc2dbe9984fb85234db9adf",
     prompt: "Mona Lisa",
     images: [
-      { image: "3/fa0fa4c-d596e44.jpg", ai: "de" },
-      { image: "3/fa0fa4c-a191e45.png", ai: "sd" },
-      { image: "blah.jpg", ai: "mj" },
+      { image: "3/fa0fa4c-215bd1f.jpg", ai: "de" },
+      { image: "3/fa0fa4c-a29d6e6.png", ai: "sd" },
+      { image: "3/fa0fa4c-e5782a0.png", ai: "mj" },
     ],
   },
   {
-    id: "c1a251711f0c7fbb13e586a953b7c294",
-    prompt: "Portrait of an albino with long hairs, no clothes",
+    id: "5b54420a8c20fab3825c3f0484ff9074",
+    prompt: "Portrait of an albino with long hairs",
     images: [
-      { image: "4/c1a2517-1efd3b9.jpg", ai: "de" },
-      { image: "4/c1a2517-02c9bf7.png", ai: "sd" },
-      { image: "blah.jpg", ai: "mj" },
+      { image: "4/5b54420-4b52804.jpg", ai: "de" },
+      { image: "4/5b54420-72dbd9f.png", ai: "sd" },
+      { image: "4/5b54420-19714ff.png", ai: "mj" },
     ],
   },
   {
     id: "2e323d3644d20d5c4b626df3d222115a",
     prompt: "A beautiful princess with a blue umbrella and a red cupcake",
     images: [
-      { image: "5/2e323d3-4dea468.jpg", ai: "de" },
-      { image: "5/2e323d3-00f5bc7.png", ai: "sd" },
-      { image: "blah.jpg", ai: "mj" },
+      { image: "5/2e323d3-cbf3be0.jpg", ai: "de" },
+      { image: "5/2e323d3-bcc1145.png", ai: "sd" },
+      { image: "5/2e323d3-9ef451b.png", ai: "mj" },
     ],
   },
   {
     id: "1ea4edc342731a6e7edec3065645c2f6",
     prompt: "A cupcake shop for dogs",
     images: [
-      { image: "6/1ea4edc-50087fa.jpg", ai: "de" },
-      { image: "6/1ea4edc-a6c17ea.png", ai: "sd" },
-      { image: "blah.jpg", ai: "mj" },
+      { image: "6/1ea4edc-5ce9d11.jpg", ai: "de" },
+      { image: "6/1ea4edc-5aaff12.png", ai: "sd" },
+      { image: "6/1ea4edc-769d76b.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "c341a052d0bcf02e3c35cade7a4696ed",
+    prompt:
+      "Freeform ferrofluids, beautiful dark chaos, swirling black frequency",
+    images: [
+      { image: "7/c341a05-c039976.jpg", ai: "de" },
+      { image: "7/c341a05-eca437e.png", ai: "sd" },
+      { image: "7/c341a05-ef0d0e0.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "af159d340c272b57224725c25ea4759d",
+    prompt: "A bus stop in the middle of the ocean, HD, hyper realistic",
+    images: [
+      { image: "8/af159d3-abc0bf4.jpg", ai: "de" },
+      { image: "8/af159d3-a57cfdc.png", ai: "sd" },
+      { image: "8/af159d3-ebf38ed.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "12211628a4470bc9a0761b6111e78d89",
+    prompt:
+      "Rubber Duck Aliens visiting the Earth for the first time, hyper-realistic, cinematic, detailed",
+    images: [
+      { image: "9/1221162-2e23416.jpg", ai: "de" },
+      { image: "9/1221162-c4d18a4.png", ai: "sd" },
+      { image: "9/1221162-45f044b.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "986e35f1c80cbef99d2420bfee839af5",
+    prompt: "dodecahedron Floating in space, hyperrealistic, GODLIKE, bokeh",
+    images: [
+      { image: "10/986e35f-4342a91.jpg", ai: "de" },
+      { image: "10/986e35f-7616c6f.png", ai: "sd" },
+      { image: "10/986e35f-d80c737.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "5aa1c6499c636105c47023a29cd931d8",
+    prompt:
+      "Black and white geometric drawing, lines, symmetry, mystical, magic, dirty",
+    images: [
+      { image: "11/5aa1c64-1d1c83f.jpg", ai: "de" },
+      { image: "11/5aa1c64-b0ae102.png", ai: "sd" },
+      { image: "11/5aa1c64-f0d25fb.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "e2193374f6c52785edcd4685d03253e0",
+    prompt:
+      "viking north druid lich mermaid king wise old man god of death witch pagan face portrait, underwater, covered in runes, crown made of bones, necromancer, zdzisław beksiński, mikhail vrubel, hr giger, gustav klimt, symmetry, mystical occult symbol",
+    images: [
+      { image: "12/e219337-2057c4b.jpg", ai: "de" },
+      { image: "12/e219337-57ad295.png", ai: "sd" },
+      { image: "12/e219337-7f4a5c2.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "b7a366b30aaf1df5ea1aea726340430a",
+    prompt:
+      "Samhain figure, creature, wicca, occult, harvest, fall, hyper-realistic, ultra resolution, creepy, dark, witchcore",
+    images: [
+      { image: "13/b7a366b-bbe737a.jpg", ai: "de" },
+      { image: "13/b7a366b-0d76fed.png", ai: "sd" },
+      { image: "13/b7a366b-9aee9d0.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "8fb4ea012141ffb20abe3f192b774b46",
+    prompt: "seamless tileable jade tree pattern, spiral carvings",
+    images: [
+      { image: "14/8fb4ea0-0ee0e21.jpg", ai: "de" },
+      { image: "14/8fb4ea0-4f07103.png", ai: "sd" },
+      { image: "14/8fb4ea0-309c962.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "c061e879ace3cee7349f0037c8092a59",
+    prompt:
+      "Lovecraft character Cthulhu, with the hunter hat, and the saw cleaver, with bloodborne weapons, full body, in the style bloodborne style, full body, dark fantasy, trending on ArtStation",
+    images: [
+      { image: "15/c061e87-182de4f.jpg", ai: "de" },
+      { image: "15/c061e87-66a056d.png", ai: "sd" },
+      { image: "15/c061e87-9bfea0b.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "b27cdcd8fc2c06be4dc158bad87e9cc2",
+    prompt:
+      "photorealistic flying house, many details, Ultra detailed, octane render",
+    images: [
+      { image: "16/b27cdcd-d1a5440.jpg", ai: "de" },
+      { image: "16/b27cdcd-427079f.png", ai: "sd" },
+      { image: "16/b27cdcd-2faf71b.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "ab766e944514af1f0f68e65e59783dec",
+    prompt:
+      "Da Vinci drawing of a revolutionary exoskeleton, many details, high resolution, hand drawn",
+    images: [
+      { image: "17/ab766e9-e24fe05.jpg", ai: "de" },
+      { image: "17/ab766e9-22eaaba.png", ai: "sd" },
+      { image: "17/ab766e9-675a059.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "d083ffaf932a352626d3e76408835057",
+    prompt:
+      "full page technical drawing technocore mind meld evil-god symmetric, Hieroglyphic, Occult esoterism, hyper realistic, rendered, 8K, old, neon, vibrant fine details symmetric",
+    images: [
+      { image: "18/d083ffa-2426531.jpg", ai: "de" },
+      { image: "18/d083ffa-d1dcf66.png", ai: "sd" },
+      { image: "18/d083ffa-a99fed6.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "35ed93353629a4a792137459d9f3aa80",
+    prompt: "Wet texture, greasy, strange, unsettling, 3d texture",
+    images: [
+      { image: "19/35ed933-6854185.jpg", ai: "de" },
+      { image: "19/35ed933-e985051.png", ai: "sd" },
+      { image: "19/35ed933-fbfe340.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "774574d0dc2d03bfe4327e54dc81de12",
+    prompt: "Globe half earth half orange floating in smoke",
+    images: [
+      { image: "20/774574d-4f2abe9.jpg", ai: "de" },
+      { image: "20/774574d-9c6c003.png", ai: "sd" },
+      { image: "20/774574d-80c333f.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "547735175b016d967eef5273c80cd58d",
+    prompt:
+      "dense woodland landscape , English forest, Irish forest, scottish forest, perspective, folklore,King Arthur, Lord of the Rings, Game of Thrones. ultra photoreal , photographic, concept art, cinematic lighting, cinematic composition, rule of thirds , mysterious, eerie, cinematic lighting, ultra-detailed, ultrarealistic, photorealism, 8k",
+    images: [
+      { image: "21/5477351-d90abd1.jpg", ai: "de" },
+      { image: "21/5477351-e83a650.png", ai: "sd" },
+      { image: "21/5477351-5c9a963.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "cf3f1a7b90a4e5505df9d3421fdf046e",
+    prompt:
+      "Portrait In the style of Rembrandt Harmenszoon van Rijn oil painting",
+    images: [
+      { image: "22/cf3f1a7-193cab7.jpg", ai: "de" },
+      { image: "22/cf3f1a7-cba6c41.png", ai: "sd" },
+      { image: "22/cf3f1a7-84e1959.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "fb56f3830e4a9ce49e13d9df9be080f6",
+    prompt:
+      "A string structure, made of silver, enclosing a silicone translucent humanoïde creature, large shot, panoramic, black and white, with a glimpse of red, with a glimpse of green, atmospheric, intricate fine ornemental, cinematic, hyper realism, mono directional light, Caustics, blade runner, David Lynch inspiration, Terry Gilliam inspiration, Luis Bunuel inspiration, Jean Jeunet inspiration, crystalcore, high detail octane redshift Lumion render 8k, concept art, hyper detailed, insane details, intricate, elite, ornate, elegant, luxury, realist, golden ratio, octane render, weta digital, ray trace, 8k",
+    images: [
+      { image: "23/fb56f38-ade4c66.jpg", ai: "de" },
+      { image: "23/fb56f38-e9f46d8.png", ai: "sd" },
+      { image: "23/fb56f38-a376044.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "0e6485aab3a02911474af2db274f5137",
+    prompt:
+      "pencil drawing of an insect, abstract, surrealism, hyper detail, line art",
+    images: [
+      { image: "24/0e6485a-28886dd.jpg", ai: "de" },
+      { image: "24/0e6485a-b68a6e2.png", ai: "sd" },
+      { image: "24/0e6485a-bfdc563.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "033c2ce692a3d4358d14f19a5a07780d",
+    prompt:
+      "in darkest blue subway,a side road of huge underground sewage channel, wallpaper",
+    images: [
+      { image: "25/033c2ce-ce2c932.jpg", ai: "de" },
+      { image: "25/033c2ce-0754506.png", ai: "sd" },
+      { image: "25/033c2ce-a43f8ce.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "f61cdac856c44a1dc00c12de9f039165",
+    prompt:
+      "Japanese forest spirit, qirin, god, deity, illustrated, detailed, serene",
+    images: [
+      { image: "26/f61cdac-38f7b96.jpg", ai: "de" },
+      { image: "26/f61cdac-aa4a4b9.png", ai: "sd" },
+      { image: "26/f61cdac-387bf1c.png", ai: "mj" },
+    ],
+  },
+  {
+    id: "3fb3ca43a80d194bebf78d475f8d6f41",
+    prompt: "Mountain, river, pink",
+    images: [
+      { image: "27/3fb3ca4-14fa048.jpg", ai: "de" },
+      { image: "27/3fb3ca4-66bd9e8.png", ai: "sd" },
+      { image: "27/3fb3ca4-3aee233.png", ai: "mj" },
     ],
   },
 ];
